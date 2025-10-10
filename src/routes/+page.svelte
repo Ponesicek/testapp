a2df2f4e3eb81da5d659c0569b42f9289e2923f0
ponesicek <62983557+Ponesicek@users.noreply.github.com>
2025-10-10 14:51:56 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<p>This is a test push 2</p>
+<p>This is a test push 3</p>
